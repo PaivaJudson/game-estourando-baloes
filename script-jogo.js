e9e71cb5b541bde99fc2aff950b7cb90c344d996
Judson Paiva <judson.paiva@hotmail.com>
2023-05-03 08:26:32 +0100

    associando dinamicamente baloes

--- script-jogo.js
+++ script-jogo.js
@@ -5,6 +5,9 @@ function iniciarJogo(){
     //var level = url.toString()[url.toString().length-1];
     var nivel_jogo = url.replace('?', "");
     var tempo_segundos = 0;
+    var qtde_baloes = 10;
+    var baloes_estourados = 5;
+
 
     if(nivel_jogo == 1){
         tempo_segundos = 120;
@@ -17,9 +20,18 @@ function iniciarJogo(){
     }
 
     document.getElementById('cronometro').innerHTML = tempo_segundos;
-
-    var qtde_baloes = 10;
+    document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+    document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
 
     cria_baloes(qtde_baloes);
 }
 
+function cria_baloes(qtde_baloes){
+    
+    for (let index = 0; index < qtde_baloes; index++) {
+        var balao = document.createElement('img');
+        balao.src = 'imagens/balao_azul_pequeno.png';
+        balao.style.margin = '10px';
+        document.getElementById('cenario').appendChild(balao);
+    }
+}
